8102efab6c3279a66483ac50b231a270ab18be2e
joshuaramin <raminjoshua05@gmail.com>
2023-05-23 08:59:03 +0800

    fixed email

--- src/api/helpers/email.ts
+++ src/api/helpers/email.ts
@@ -47,10 +47,10 @@ export async function Recipient(Email: string, message: string, subject: string)
         auth: {
             type: "OAuth2",
             user: "gspecialistinc@gmail.com", //gmail account you useds to set the project up in google cloud console.
-            clientId: "782394693386-gooll1na41pkftagsb1np2d9scleeqfg.apps.googleusercontent.com", // ClientID 
-            clientSecret: "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", // Client Secret
-            refreshToken: "1//04FXnUOlIxqa7CgYIARAAGAQSNwF-L9Iro9-YzzVboDHkKEZ_5_QTPdfnSVOROlQ9dpBdOJcH4Skc_vFwM1um5nYpGhSaj_CEC4Y", // Refresh Token
-            accessToken: "ya29.a0AVvZVsqC9ZrADOemh-V5dBo27O-2-0ohnjDtYhX2UIDfTHoNnKmh_fnibn4XLYAymrPQrazTfaPszQRCr0hOe_ipxdzAAqeKRLiAQbR0qLOVVlAyeZgI9lC7PlByFAwIiYXx9Za3XVixJ7XRCjAxzmvpKPTBaCgYKAdcSARMSFQGbdwaIj99uD19-4a3GlJihv82DCQ0163" // Access Token Variable we defined earlier
+            clientId: "323850014625-9bkvkgdb48jr6lctd9oam93mnegsjoeg.apps.googleusercontent.com", // ClientID 
+            clientSecret: "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", // Client Secret
+            refreshToken: "1//04k3t7H0QzLfhCgYIARAAGAQSNwF-L9IrzOSIthKv6sBgaB74yqloWf0R-yRkTgINk3BMOrz21L2ZQRCmQH5fTEIRuv1qMXsqV-k", // Refresh Token
+            accessToken: "ya29.a0AWY7Ckljugt-NOTaQzOSuptcUGpmfKP6l3_N-4DIr9Uwev190b4Noruwlih_UMsb58_X0RobGjzCQtFgGKe3fqbswOzv2VFJVSXjrTo3hm-qvTkyUkhQzokmCjaC13ZC1XBKIEzyj5IORusfu7-xqai09YH9aCgYKAZcSARISFQG1tDrpukSTLtNadTmT6iD8ZKLLGA0163" // Access Token Variable we defined earlier
         }
     })
 
